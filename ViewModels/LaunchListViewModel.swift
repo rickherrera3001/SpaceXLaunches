cfcc1511d16ec0285b46cfa96df3d07b7619fb7f
Ricardo Ivan Herrera Rocha <rihr.3001@gmail.com>
2025-06-01 21:33:57 -0600

    feat(LaunchList): Implement LaunchListViewModel

--- ViewModels/LaunchListViewModel.swift
+++ ViewModels/LaunchListViewModel.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import SwiftData
+
+@MainActor
+class LaunchListViewModel: ObservableObject {
+    @Published var launches: [LaunchEntity] = []
+    @Published var isLoading = false
+    @Published var error: String?
+
+    func loadLaunchesIfNeeded(modelContext: ModelContext) {
+        isLoading = true
+        error = nil
+
+        LaunchService.shared.fetchLaunchesIfNeeded(modelContext: modelContext) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.isLoading = false
+                switch result {
+                case .success:
+                    do {
+                        let descriptor = FetchDescriptor<LaunchEntity>(sortBy: [.init(\.launchDateUTC, order: .reverse)])
+                        self?.launches = try modelContext.fetch(descriptor)
+                    } catch {
+                        self?.error = "Failed to load saved launches"
+                    }
+                case .failure(let err):
+                    self?.error = err.localizedDescription
+                }
+            }
+        }
+    }
+}
